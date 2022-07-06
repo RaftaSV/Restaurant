d287ab9eb7b7281c2ee39d439c41aa56b7974e03
RaftaSV <62962896+RaftaSV@users.noreply.github.com>
2022-07-05 22:11:03 -0600

    ajuste

---
+++ src/components/Atoms/ButtonMenu/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const StyleWrapper = styled.div`
+  display: flex;
+  width: auto;
+  height: 50px;
+  background: green;
+  align-items: center;
+  justify-content: center;
+  font-size: 30px;
+`;
--- src/components/Molecules/Modals/modalMenu.js
+++ src/components/Molecules/Modals/modalMenu.js
@@ -1,7 +1,11 @@
 import Modal from 'components/Atoms/Modal';
 import { StyleCloseBtn } from './style';
+import ButtonMenu from 'components/Atoms/ButtonMenu';
+import {useTranslation} from 'react-i18next';
 
 const ModalMenu = ({ visible, onCancel }) => {
+  const {t} = useTranslation();
+  const separator = t('SpecialDish');
   return (
     <Modal
       closeButton={false}
@@ -15,20 +19,10 @@ const ModalMenu = ({ visible, onCancel }) => {
         X
       </StyleCloseBtn>
 
-      <h1>Platos especiales</h1>
-      <br/>
-      <h1>Mariscos</h1>
-      <br/>
-      <h1>Pollo</h1>
-      <br/>
-      <h1>Carne</h1>
-      <br/>
-      <h1>Tipicos</h1>
-      <br/>
-      <h1>Otros</h1>
-      <br/>
-      <h1>Bebidas</h1>
-
+     <ButtonMenu Item={separator}></ButtonMenu>
+      <ButtonMenu Item={'Mariscos'}></ButtonMenu>
+      <ButtonMenu Item={'Otros'}></ButtonMenu>
+      <ButtonMenu Item={'Bebidas'}></ButtonMenu>
     </Modal>
   );
 };
