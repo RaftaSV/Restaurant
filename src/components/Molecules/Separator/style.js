75b6ce8698861193269b3f6a0f6fdc74242f9c7d
RaftaSV <62962896+RaftaSV@users.noreply.github.com>
2022-07-09 15:00:31 -0600

    ajustes

--- src/components/Molecules/Separator/style.js
+++ src/components/Molecules/Separator/style.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const StyleWrapper = styled.div`
-  margin-top: 1%;
+  margin-top: 3%;
   background: ${({ theme }) => theme.colors.Separator};
   height: max-content;
   border-radius: 10px;
