ca0a5c2ca3c48d886d645c1b5b8e0520c4cd1d61
RaftaSV <62962896+RaftaSV@users.noreply.github.com>
2022-07-09 15:04:53 -0600

    Ajustes

--- src/components/Molecules/Separator/style.js
+++ src/components/Molecules/Separator/style.js
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 export const StyleWrapper = styled.div`
   margin-top: 3%;
   background: ${({ theme }) => theme.colors.Separator};
-  height: max-content;
   border-radius: 10px;
-
+  height: max-content;
+  @media screen and (min-width: 768px) {
+    margin-top: 1%;
+  }
 `;
